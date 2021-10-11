7bd1a635853060f7ef9932e1b2d7958f9a219089
pedroaz <pedro.azvm@gmail.com>
2021-10-11 14:18:08 -0300

    Get data from people table

--- ConsumerCity.AwsManager.Func/Function.cs
+++ ConsumerCity.AwsManager.Func/Function.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Runtime;
+using Amazon.DynamoDBv2;
+using Amazon;
+using Amazon.DynamoDBv2.DataModel;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -48,17 +52,55 @@ namespace ConsumerCity.AwsManager.Func
         /// </summary>
         /// <param name="request"></param>
         /// <returns>The API Gateway response.</returns>
-        public APIGatewayProxyResponse MySecondFunc(APIGatewayProxyRequest request, ILambdaContext context)
+        public async Task<APIGatewayProxyResponse> MySecondFunc(APIGatewayProxyRequest request, ILambdaContext context)
         {
-            context.Logger.LogLine("Get Request\n");
+            try {
+                var credentials = new BasicAWSCredentials(Environment.GetEnvironmentVariable("DynamoUsername"), Environment.GetEnvironmentVariable("DynamoPassword"));
+                var config = new AmazonDynamoDBConfig() {
+                    RegionEndpoint = RegionEndpoint.SAEast1
+                };
+                var client = new AmazonDynamoDBClient(credentials, config);
 
-            var response = new APIGatewayProxyResponse {
-                StatusCode = (int)HttpStatusCode.OK,
-                Body = "This is my second function",
-                Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } }
-            };
+                var dbContext = new DynamoDBContext(client);
 
-            return response;
+                //var result = await context.LoadAsync<Person>("1");
+
+                //await context.SaveAsync(new Person() { Id = "1", Name = "Pedro"});
+
+                var result = await dbContext.ScanAsync<Person>(new List<ScanCondition>()).GetRemainingAsync();
+
+                return new APIGatewayProxyResponse {
+                    StatusCode = (int)HttpStatusCode.OK,
+                    Body = result.First().Name,
+                    Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } }
+                };
+            }
+            catch (Exception e) {
+
+                return new APIGatewayProxyResponse {
+                    StatusCode = (int)HttpStatusCode.BadRequest,
+                    Body = "error",
+                    Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } }
+                };
+            }
+        }
+
+        public class Person
+        {
+            [DynamoDBHashKey("id")]
+            public string Id { get; set; }
+
+            [DynamoDBProperty("name")]
+            public string Name { get; set; }
+
+            [DynamoDBProperty("Car")]
+            public Car Car { get; set; }
+        }
+
+        public class Car
+        {
+            public string Name { get; set; }
+            public string Value { get; set; }
         }
     }
 }
